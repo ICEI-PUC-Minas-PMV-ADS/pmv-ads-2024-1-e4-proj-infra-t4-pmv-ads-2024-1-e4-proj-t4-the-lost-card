f15fdfb47ad22cab50b142b4a485779e44218fe7
LAPTOP-P1LBD27N\Littlebone <luizeduardodejes@gmail.com>
2024-03-30 21:06:02 -0300

    feat: Adicionando signup

---
+++ src/backend/Application/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    private static readonly Type _thisType = typeof(DependencyInjection);
+    public static IServiceCollection AddLostCardsApp(this IServiceCollection services)
+    {
+        services.AddSingleton<ICryptographyService, CryptographyService>();
+
+        services.AddMediatR(cfg => cfg
+            .RegisterServicesFromAssemblyContaining(_thisType)
+        );
+
+        return services;
+    }
+}
