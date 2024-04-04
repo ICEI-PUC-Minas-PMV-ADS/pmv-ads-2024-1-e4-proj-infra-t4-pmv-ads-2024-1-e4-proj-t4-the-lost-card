1537f831fca3b2693e1d144fb6c0d1061c690263
Adeilton-Ro <adeiltonjr15@gmail.com>
2024-04-04 19:16:11 -0300

    Adicionando pipeline de busca de metadados da requisição

---
+++ src/backend/Application/Behaviours/RequestInfoBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using Application.Services;
+using FluentResults;
+using MediatR;
+
+namespace Application.Behaviours;
+
+class RequestInfoBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : MediatR.IRequest<TResponse>, IRequestMetadata
+    where TResponse : ResultBase, new()
+{
+    private readonly IRequestInfoService requestInfoService;
+
+    public RequestInfoBehaviour(IRequestInfoService requestInfoService)
+    {
+        this.requestInfoService = requestInfoService;
+    }
+
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        request.RequestInfo = await requestInfoService.SetRequestInfo(cancellationToken);
+        return await next();
+    }
+}
