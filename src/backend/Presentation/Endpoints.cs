bf73cb00b927b5fab796a28342eacf434df9399d
LAPTOP-P1LBD27N\Littlebone <luizeduardodejes@gmail.com>
2024-04-04 22:02:32 -0300

    Adicionando tratamento de erros

--- src/backend/Presentation/Endpoints.cs
+++ src/backend/Presentation/Endpoints.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Presentation.Serialization;
 using System.Threading.Tasks;
 
 namespace Presentation;
@@ -20,14 +21,6 @@ public static class Endpoints
         
         var responseResult = await req.HttpContext.RequestServices.GetService<ISender>().Send(request, cancellationToken);
 
-        if (responseResult.IsSuccess)
-            return new ObjectResult(responseResult.Value);
-
-        var problemDetails = new ProblemDetails()
-        {
-            Detail = "Algum problema ocorreu"
-        };
-
-        return new ObjectResult(problemDetails) { StatusCode = StatusCodes.Status403Forbidden };
+        return HttpSerialization.Serialize(responseResult);
     }
 }
---
+++ src/backend/Presentation/Serialization/HttpSerialization.cs
@@ -0,0 +1,44 @@
+﻿using Application.FluentResultExtensions;
+using FluentResults;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Presentation.Serialization;
+
+public class HttpSerialization
+{
+    public static IActionResult Serialize<TValue>(Result<TValue> result)
+    {
+        if (result is { IsSuccess: true })
+            return new ObjectResult(result.Value);
+        
+        return Serialize(result.ToResult());
+    }
+
+    public static IActionResult Serialize(Result result)
+    {
+        var firstError = result.Errors.FirstOrDefault();
+
+        if (firstError is RequestValidationError validationError)
+        {
+            var validationProblemDetails = new ValidationProblemDetails(validationError.FieldReasonDictionary)
+            {
+                Detail = firstError.Message,
+                Status = 500,
+                Title = "Validation error"
+            };
+
+            return new ObjectResult(validationProblemDetails) { StatusCode = StatusCodes.Status400BadRequest };
+        }
+
+        var problemDetails = new ProblemDetails
+        {
+            Detail = firstError.Message,
+            Status = 500,
+            Title = "Unhandled error",
+        };
+
+        return new ObjectResult(problemDetails) { StatusCode = StatusCodes.Status500InternalServerError };
+    }
+}
