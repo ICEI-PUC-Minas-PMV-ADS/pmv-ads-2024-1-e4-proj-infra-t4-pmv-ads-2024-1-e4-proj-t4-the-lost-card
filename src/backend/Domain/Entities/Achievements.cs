ec44a6c96949459cfce5a2938b3cd30911ba60ba
gustavo.h <gustavo.henrique@protechsolutions.com.br>
2024-04-07 19:14:43 -0300

    adicinando nova entidade e relacionando com player

---
+++ src/backend/Domain/Entities/Achievements.cs
@@ -0,0 +1,7 @@
+﻿namespace Domain.Entities;
+
+public class Achievements : Entity
+{
+    public string Name { get; set; } = string.Empty;
+    public string Descriptions { get; set; } = string.Empty;
+}
