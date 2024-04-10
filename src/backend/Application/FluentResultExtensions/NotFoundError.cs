81105f5d03ca546be9fac942852f67da496ad7e4
Adeilton-Ro <adeiltonjr15@gmail.com>
2024-04-10 19:43:47 -0300

    Adicionando erro de não encontrado

---
+++ src/backend/Application/FluentResultExtensions/NotFoundError.cs
@@ -0,0 +1,8 @@
+﻿using FluentResults;
+
+namespace Application.FluentResultExtensions;
+
+public class NotFoundError : Error
+{
+    public NotFoundError(string message = "not found") : base(message) { }
+}
