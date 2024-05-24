3a3f6e8b3775cb10fce2f06105adffcfec89a242
Adeilton-Ro <adeiltonjr15@gmail.com>
2024-05-24 20:43:53 -0300

    Adicionando modal

---
+++ src/frontendMobile/App/Components/Modal.tsx
@@ -0,0 +1,34 @@
+import {StyleSheet, View} from 'react-native';
+import LostCardIcon from '../Assets/logo.svg';
+import React from 'react';
+import {ViewProps} from 'react-native-svg/lib/typescript/fabric/utils';
+
+const LostCardInput: React.FC<ViewProps> = ({children, ...props}) => {
+  return (
+    <View style={styles.container} {...props}>
+      <LostCardIcon width={'40%'}/>
+      <View style={styles.verticleLine} />
+      <View>{children}</View>
+    </View>
+  );
+};
+
+export default LostCardInput;
+
+const styles = StyleSheet.create({
+  container: {
+    width: '80%',
+    height: '75%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: '#5E5C72',
+    borderRadius: 10,
+    padding: 20
+  },
+  verticleLine: {
+    height: '90%',
+    width: 1,
+    backgroundColor: '#909090',
+  },
+});
