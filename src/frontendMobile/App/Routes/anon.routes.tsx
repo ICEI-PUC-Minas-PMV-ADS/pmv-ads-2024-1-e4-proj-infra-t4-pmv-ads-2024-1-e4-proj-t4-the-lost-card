2b8fc8a6884052e0593f8a4e6940d93af52e9653
pedro_orafael <pedro_orafael@DESKTOP-BT454AU>
2024-05-24 22:21:11 -0300

    Adicionando homePage e SplashPage

--- src/frontendMobile/App/Routes/anon.routes.tsx
+++ src/frontendMobile/App/Routes/anon.routes.tsx
@@ -1,19 +1,19 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import DefaultAnnonPage from '../Pages/DefaultAnnonPage';
 // import SignInPage from '../Pages/SignInPage';
-// import SplashPage from '../Pages/SplashPage';
-// import HomePage from '../Pages/HomePage';
+ import SplashPage from '../Pages/SplashPage';
+ import HomePage from '../Pages/HomePage';
 // import SignUpPage from '../Pages/SignUpPage';
 
 const {Screen, Navigator} = createNativeStackNavigator();
 
 const AnonRoutes = () => {
     return (
-        <Navigator screenOptions={{ headerShown: false }} initialRouteName='DefaultAnnon'>
-            <Screen name="DefaultAnnon" component={DefaultAnnonPage} /> 
-            {/* <Screen name="SplashPage" component={SplashPage} />
+        <Navigator screenOptions={{ headerShown: false }}initialRouteName='SplashPage'>
+            {/* <Screen name="DefaultAnnon" component={DefaultAnnonPage} />  */}
+            <Screen name="SplashPage" component={SplashPage} />
             <Screen name="HomePage" component={HomePage} />
-            <Screen name="SignInPage" component={SignInPage} />
+            {/* <Screen name="SignInPage" component={SignInPage} />
             <Screen name="SingUpPage" component={SignUpPage} /> */}
         </Navigator>
     )
