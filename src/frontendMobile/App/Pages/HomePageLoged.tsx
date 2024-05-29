603ee762febac9438f3ade7e5281bbb11b1b0b8d
Adeilton-Ro <adeiltonjr15@gmail.com>
2024-05-28 21:22:58 -0300

    Adicionando home page depois de logado

---
+++ src/frontendMobile/App/Pages/HomePageLoged.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {
+  BackHandler,
+  Image,
+  Platform,
+  Pressable,
+  StyleSheet,
+} from 'react-native';
+import Anchor from '../Components/Anchor';
+import Background from '../Components/Background';
+import ListItem from '../Components/ListItem';
+
+const HomePageLoged: React.FC = () => {
+  const exit = () => {
+    if (Platform.OS === 'android') {
+      if (BackHandler) {
+        BackHandler.exitApp();
+      }
+    }
+  };
+
+  return (
+    <Background style={styles.container}>
+      <Image source={require('../Assets/Logo.png')} style={styles.logo} />
+
+      <Anchor text="Buscar salas" route="GameRoomRouter" />
+      <Pressable onPress={exit}>
+        <ListItem text="Sair" />
+      </Pressable>
+    </Background>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'flex-start',
+    justifyContent: 'flex-end',
+  },
+  logo: {
+    position: 'relative',
+    left: 380,
+    top: 50,
+    width: 200,
+    height: 200,
+    resizeMode: 'contain',
+  },
+});
+
+export default HomePageLoged;
--- src/frontendMobile/App/Routes/app.routes.tsx
+++ src/frontendMobile/App/Routes/app.routes.tsx
@@ -2,6 +2,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import GameRoomDebugHelper from '../Pages/GameRoomDebugHelper';
 import { GameRoomRouter } from "../Pages/GameRoom"
 import { LobbySearch } from '../Pages/GameRoom/LobbySearch';
+import { Lobby } from '../Pages/GameRoom/Lobby';
+import HomePageLoged from '../Pages/HomePageLoged';
 // import MainMenuPage from '../Pages/MainMenuPage';
 // import QueryRoomsPage from '../Pages/QueryRooms';
 // import CreateRoom from '../Pages/CreateRoom';
@@ -10,13 +12,11 @@ const { Screen, Navigator } = createNativeStackNavigator();
 
 const AppRoutes = () => {
   return (
-    <Navigator screenOptions={{ headerShown: false }} initialRouteName='GameRoomRouter'>
+    <Navigator screenOptions={{ headerShown: false }} initialRouteName='HomePage'>
       <Screen name="DebugHelper" component={GameRoomDebugHelper} />
       <Screen name="LobbySearch" component={LobbySearch} />
-      <Screen name="GameRoomRouter" component={GameRoomRouter}></Screen>
-      {/* <Screen name="MainMenuPage" component={MainMenuPage} />
-      <Screen name="QueryRooms" component={QueryRoomsPage} />
-      <Screen name="CreateRoom" component={CreateRoom} /> */}
+      <Screen name="GameRoomRouter" component={GameRoomRouter} />
+      <Screen name="HomePage" component={HomePageLoged} />
     </Navigator>
   );
 };
